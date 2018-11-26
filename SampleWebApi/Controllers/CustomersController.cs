54768a1d7b79715630cb2ad26a51d6f7e3046adb
Ayman Gassara <a.gassara@root.sa>
2018-11-26 09:33:44 +0100

    Add project files.

---
+++ SampleWebApi/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using SampleWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SampleWebApi.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        // GET: api/Customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return Data.MockData.CustomersList;
+        }
+
+        // GET: api/Customers
+        public Customer GetCustomersByID(int id)
+        {
+            return Data.MockData.CustomersList.Find(x=>x.Id == id);
+        }
+
+        // Post: api/Customers
+        public void PostCustomers(Customer customers)
+        {
+           Data.MockData.CustomersList.Add(customers);
+        }
+
+        // Put: api/Customers
+        public void PutCustomers(int id , Customer customers)
+        {
+            var customer = Data.MockData.CustomersList.Find(x => x.Id == id);
+            if (customer != null)
+            {
+                Data.MockData.CustomersList.Remove(customer);
+                Data.MockData.CustomersList.Add(customers);
+            }
+        }
+
+        public void DeleteCustomers(int id)
+        {
+            var customer = Data.MockData.CustomersList.Find(x => x.Id == id);
+            Data.MockData.CustomersList.Remove(customer);
+        }
+    }
+}
---
+++ SampleWebApi/Data/MockData.cs
@@ -0,0 +1,21 @@
+﻿using SampleWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleWebApi.Data
+{
+    public static class MockData
+    {
+        public static List<Customer> CustomersList { get; set; } = new List<Customer>()
+        {
+            new Customer {Id = 0, Adress="Sfax 5 august", FirstName="Ayman" , LastName ="Gassara" },
+             new Customer {Id = 1, Adress="Sfax 5 august", FirstName="Ayman" , LastName ="Gassara" },
+              new Customer {Id = 2, Adress="Sfax 5 august", FirstName="Ayman" , LastName ="Gassara" },
+               new Customer {Id = 3, Adress="Sfax 5 august", FirstName="Ayman" , LastName ="Gassara" },
+                new Customer {Id = 4, Adress="Sfax 5 august", FirstName="Ayman" , LastName ="Gassara" },
+                 new Customer {Id = 5, Adress="Sfax 5 august", FirstName="Ayman" , LastName ="Gassara" },
+        };
+    }
+}
